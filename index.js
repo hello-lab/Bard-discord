ddb0687ad7814838d36c225e2e22950ab1121236
hello-lab <58252733+hello-lab@users.noreply.github.com>
2023-07-18 13:50:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,12 @@ const express =require('express')
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) {
+	
+	client.on('ready', () => {
+  res.send('Logged in as ${client.user.tag}`);
+});
+	});
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
 
